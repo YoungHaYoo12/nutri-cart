2b2e3edaf1a22c732565773e47e308215c9da352
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-05 13:29:16 +0000

    Add SearchForm to index.html

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app.core import core 
+from app.core.forms import SearchForm
 
-@core.route('/')
+@core.route('/', methods=['GET','POST'])
 def index():
-  return render_template('main/index.html')
+  form = SearchForm()
+  if form.validate_on_submit():
+    return redirect(url_for('core.index'))
+
+  return render_template('main/index.html',form=form)
