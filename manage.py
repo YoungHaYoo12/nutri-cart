192e95690ea9c9ebc81220e8595b387143772c84
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-09 01:21:25 +0000

    Add coverage report functionality

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate
 from app import create_app, db
@@ -6,12 +7,36 @@ app = create_app('default')
 manager = Manager(app)
 Migrate(app,db)
 
+########################
+#Coverage SetUp
+if os.environ.get('FLASK_COVERAGE'):
+  import coverage
+  COV = coverage.coverage(branch=True,include='app/*')
+  COV.start()
+########################
+
 @manager.command
-def test():
+def test(coverage=False):
   """Run the Unit Tests"""
+  if coverage and not os.environ.get('FLASK_COVERAGE'):
+    import sys 
+    os.environ['FLASK_COVERAGE'] = '1'
+    os.execvp(sys.executable, [sys.executable] + sys.argv)
+
   import unittest
   tests = unittest.TestLoader().discover('tests')
   unittest.TextTestRunner(verbosity=2).run(tests)
+
+  if COV:
+    COV.stop()
+    COV.save()
+    print('Coverage Summary:')
+    COV.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir,'tmp/coverage')
+    COV.html_report(directory=covdir)
+    print('HTML version: file://%s/index.html'%covdir)
+    COV.erase()
 
 if __name__ == '__main__':
   manager.run()
