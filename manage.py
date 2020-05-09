e13e258ed6a3ae17eaec7100339589dcf93203ab
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-09 00:53:58 +0000

    Add manager command to run unit tests

--- manage.py
+++ manage.py
@@ -6,5 +6,12 @@ app = create_app('default')
 manager = Manager(app)
 Migrate(app,db)
 
+@manager.command
+def test():
+  """Run the Unit Tests"""
+  import unittest
+  tests = unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
 if __name__ == '__main__':
   manager.run()
