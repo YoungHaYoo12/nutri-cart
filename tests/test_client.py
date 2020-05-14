243f19107767ebc13e1516f1746f9a371ddb3a4d
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-14 14:49:39 +0000

    Modify unit tests according to decimal changes in foods/views.py.
    - Add unit test for helper function, round_food_data
    - Change error handling results
    - Change unit tests to check specifically for decimals with two decimal
    places
    - Adjust input into helper functions and view functions from normal
    floats to those of type Decimal
    - Change input of food_info to include all nutrient_categories that are
    present in the nutrient_categories array

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,5 @@
 import unittest
+from decimal import Decimal
 from flask import url_for
 from app import create_app, db
 from app.foods.views import get_measures_tuple, get_nutrient_multiplier,update_nutrients,clean_food_data
@@ -55,40 +56,47 @@ class FlaskFoodsTestCase(FlaskClientTestCase):
     # get_measures_tuple()
       # When 'alt_measures' is None
     food_info = {
-      'serving_weight_grams':'100',
-      'serving_qty':'10',
+      'serving_weight_grams':Decimal(100),
+      'serving_qty':Decimal(10),
       'alt_measures':None,
       'serving_unit':'oz'
     }
-    self.assertTrue(get_measures_tuple(food_info) == [('10.0','oz')])
+    self.assertTrue(get_measures_tuple(food_info) == [('10.00','oz')])
 
       # When 'alt_measures' is not None
     food_info = {
       'alt_measures': [
         {
-          'serving_weight':'10',
-          'qty':'1',
+          'serving_weight':Decimal(10),
+          'qty':Decimal(1),
           'measure':'lb'
         },
         {
-          'serving_weight': '100',
-          'qty': '100',
+          'serving_weight': Decimal(100),
+          'qty': Decimal(100),
           'measure': 'g'
         }
       ]
     }
-    self.assertTrue(get_measures_tuple(food_info) == [('10.0','lb'),('1.0','g')])
+    self.assertTrue(get_measures_tuple(food_info) == [('10.00','lb'),('1.00','g')])
 
     # get_nutrient_multiplier()
-    self.assertTrue(get_nutrient_multiplier(10,20,2) == 4)
+    self.assertTrue(get_nutrient_multiplier(10,20,2) == 4.00)
     self.assertTrue(get_nutrient_multiplier(4,1,3) == 0.75)
-    self.assertTrue(type(get_nutrient_multiplier(10,20,2)) is float)
+    self.assertTrue(type(get_nutrient_multiplier(10,20,2)) is Decimal)
+    self.assertTrue(get_nutrient_multiplier('hello',0,10) == 1.00)
 
     # update_nutrients()
     food_info = {
       'nf_calories':800,
       'nf_sugars':21,
       'nf_cholesterol':10,
+      'nf_protein':0,
+      'nf_total_fat':0,
+      'nf_saturated_fat':0,
+      'nf_sodium':0,
+      'nf_total_carbohydrate':0,
+      'nf_dietary_fiber':0,
       'not_in_categories':2000,
       'not_a_number':'Hello World'
     }
@@ -104,21 +112,55 @@ class FlaskFoodsTestCase(FlaskClientTestCase):
       'serving_weight_grams':None,
       'serving_qty':'not a float'
     }
-    clean_food_data(food_info)
-    self.assertTrue(food_info['serving_weight_grams'] == 1.0)
+    clean_food_data(food_info,nutrient_categories)
+    self.assertTrue(food_info['serving_weight_grams'] == 1.00)
     self.assertTrue(food_info['serving_qty'] == 1.00)
 
     food_info = {
       'nf_calories':2000,
       'nf_sugars':'hello',
       'nf_cholesterol':None,
+      'nf_protein':0,
+      'nf_total_fat':0,
+      'nf_saturated_fat':0,
+      'nf_sodium':0,
+      'nf_total_carbohydrate':0,
+      'nf_dietary_fiber':0,      
       'nf_not_a_category':'should_not_change'
     }
-    clean_food_data(food_info)
+    clean_food_data(food_info,nutrient_categories)
     self.assertTrue(food_info['nf_calories'] == 2000) 
-    self.assertTrue(food_info['nf_sugars'] == 0) 
-    self.assertTrue(food_info['nf_cholesterol'] == 0) 
+    self.assertTrue(food_info['nf_sugars'] == 0.00) 
+    self.assertTrue(food_info['nf_cholesterol'] == 0.00) 
     self.assertTrue(food_info['nf_not_a_category'] == 'should_not_change') 
+
+    food_info = {
+      'alt_measures': [
+        {
+          'serving_weight':10,
+          'qty':10
+        },
+        {
+          'serving_weight':'notdecimal',
+          'qty':'alsonotdecimal'
+        }
+      ]
+    }
+    clean_food_data(food_info,nutrient_categories)
+    self.assertTrue(food_info['alt_measures'][0]['serving_weight'] == 10.00)
+    self.assertTrue(food_info['alt_measures'][0]['qty'] == 10.00)
+    self.assertTrue(food_info['alt_measures'][1]['serving_weight'] == 1.00)
+    self.assertTrue(food_info['alt_measures'][1]['serving_weight'] == 1.00)
+
+    # round_food_data
+    food_info = {
+      'nf_calories':20.20202020,
+      'nf_cholesterol':20
+    }
+    round_food_data(food_info,nutrient_categories)
+    self.assertTrue(food_info['nf_calories'] == 20.20)
+    self.assertTrue(food_info['nf_cholesterol'] == 20.00)
+
 
   def test_foods_common_food(self):
     # Check 404 exception for when food is not in nutritionix database
@@ -135,68 +177,68 @@ class FlaskFoodsTestCase(FlaskClientTestCase):
 
     # When url parameters serving_unit or serving_qty are None
     resp3 = self.client.get(url_for('foods.common_food',food_name='eggs'))
-    self.assertTrue('selected value="50.0"' in resp3.get_data(as_text=True))
+    self.assertTrue('selected value="50.00"' in resp3.get_data(as_text=True))
     self.assertTrue('value="1.00"' in resp3.get_data(as_text=True))
 
     # When Form Validates On Submit check redirect
     resp4 = self.client.post(url_for('foods.common_food',food_name='eggs',data = {
-      'serving_unit':'50.0',
+      'serving_unit':'50.00',
       'serving_qty':777.00
     }),follow_redirects=True)
     self.assertTrue(resp4.status_code==200)
 
     # Filling in form data when request method is GET
-    resp5 = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=63.0,serving_qty=999.0))
-    self.assertTrue('selected value="63.0"' in resp5.get_data(as_text=True))
+    resp5 = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=63.00,serving_qty=999.00))
+    self.assertTrue('selected value="63.00"' in resp5.get_data(as_text=True))
     self.assertTrue('value="999.00"' in resp5.get_data(as_text=True))    
 
     # Check nutrition updating according to serving_unit and serving_qty
     # (CHECKED AGAINST NUTRITIONIX DATABASE INFORMATION)
-    resp6a = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=50.0,serving_qty=1.00))
-    resp6b = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=50.0,serving_qty=2.00))
-    resp6c = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=38.0,serving_qty=1.00))
+    resp6a = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=50.00,serving_qty=1.00))
+    resp6b = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=50.00,serving_qty=2.00))
+    resp6c = self.client.get(url_for('foods.common_food',food_name='eggs',serving_unit=38.00,serving_qty=1.00))
     data6a = resp6a.get_data(as_text=True)
     data6b = resp6b.get_data(as_text=True)
     data6c = resp6c.get_data(as_text=True)
 
-    self.assertTrue("Calories: 71.5" in data6a)
+    self.assertTrue("Calories: 71.50" in data6a)
     self.assertTrue("Total Carbohydrate: 0.36" in data6a)
     self.assertTrue("Protein: 6.28" in data6a)
-    self.assertTrue("Sodium: 71.0" in data6a)
+    self.assertTrue("Sodium: 71.00" in data6a)
     self.assertTrue("Sugars: 0.19" in data6a)
     self.assertTrue("Total Fat: 4.76" in data6a)
     self.assertTrue("Saturated Fat: 1.56" in data6a)
-    self.assertTrue("Dietary Fiber: 0.0" in data6a)
+    self.assertTrue("Dietary Fiber: 0.00" in data6a)
 
-    self.assertTrue("Calories: 143.0" in data6b)
+    self.assertTrue("Calories: 143.00" in data6b)
     self.assertTrue("Total Carbohydrate: 0.72" in data6b)
     self.assertTrue("Protein: 12.56" in data6b)
-    self.assertTrue("Sodium: 142.0" in data6b)
+    self.assertTrue("Sodium: 142.00" in data6b)
     self.assertTrue("Sugars: 0.38" in data6b)
     self.assertTrue("Total Fat: 9.52" in data6b)
     self.assertTrue("Saturated Fat: 3.12" in data6b)
-    self.assertTrue("Dietary Fiber: 0.0" in data6b)
+    self.assertTrue("Dietary Fiber: 0.00" in data6b)
     
     self.assertTrue("Calories: 54.34" in data6c)
-    self.assertTrue("Total Carbohydrate: 0.2736" in data6c)
-    self.assertTrue("Protein: 4.7728" in data6c)
+    self.assertTrue("Total Carbohydrate: 0.27" in data6c)
+    self.assertTrue("Protein: 4.77" in data6c)
     self.assertTrue("Sodium: 53.96" in data6c)
-    self.assertTrue("Sugars: 0.1444" in data6c)
-    self.assertTrue("Total Fat: 3.6176" in data6c)
-    self.assertTrue("Saturated Fat: 1.1856" in data6c)
-    self.assertTrue("Dietary Fiber: 0.0" in data6c)
+    self.assertTrue("Sugars: 0.14" in data6c)
+    self.assertTrue("Total Fat: 3.62" in data6c)
+    self.assertTrue("Saturated Fat: 1.19" in data6c)
+    self.assertTrue("Dietary Fiber: 0.00" in data6c)
 
     # Make sure that result of get_measures_tuple is passed to select form
     resp7 = self.client.get(url_for('foods.common_food',food_name='eggs'))
     data7 = resp7.get_data(as_text=True)
-    self.assertTrue('<option value="38.0">small</option>' in data7)
-    self.assertTrue('<option value="44.0">medium</option>' in data7)
-    self.assertTrue('<option selected value="50.0">large</option>' in data7)
-    self.assertTrue('<option value="63.0">jumbo</option>' in data7)
-    self.assertTrue('<option value="56.0">extra large</option>' in data7)
-    self.assertTrue('<option value="243.0">cup (4.86 large eggs)</option>' in data7)
-    self.assertTrue('<option value="1.0">g</option>' in data7)
-    self.assertTrue('<option value="28.3495">wt. oz</option>' in data7)
+    self.assertTrue('<option value="38.00">small</option>' in data7)
+    self.assertTrue('<option value="44.00">medium</option>' in data7)
+    self.assertTrue('<option selected value="50.00">large</option>' in data7)
+    self.assertTrue('<option value="63.00">jumbo</option>' in data7)
+    self.assertTrue('<option value="56.00">extra large</option>' in data7)
+    self.assertTrue('<option value="243.00">cup (4.86 large eggs)</option>' in data7)
+    self.assertTrue('<option value="1.00">g</option>' in data7)
+    self.assertTrue('<option value="28.35">wt. oz</option>' in data7)
 
   def test_foods_branded_food(self):
     big_mac_id = "513fc9e73fe3ffd40300109f"
@@ -208,61 +250,59 @@ class FlaskFoodsTestCase(FlaskClientTestCase):
     self.assertTrue(resp1b.status_code == 200)
 
     # Error handling when url parameters "serving_unit" and "serving_qty" are not of correct type
-    resp2a = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit='10.0',serving_qty='10.0'))
-    resp2b = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit='10.0',serving_qty='not_convertable_to_float'))
+    resp2a = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit='10.00',serving_qty='10.00'))
+    resp2b = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit='10.00',serving_qty='not_convertable_to_float'))
     self.assertTrue(resp2a.status_code == 200)
     self.assertTrue(resp2b.status_code == 404)
 
     # When url parameters serving_unit or serving_qty are None
     resp3a = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id))
-    self.assertTrue('selected value="212.0"' in resp3a.get_data(as_text=True))
+    self.assertTrue('selected value="212.00"' in resp3a.get_data(as_text=True))
     self.assertTrue('value="1.00"' in resp3a.get_data(as_text=True))
 
     # When also there is a type error, make sure default values are set (serving_weight_grams of food_info is None)
     resp3b = self.client.get(url_for('foods.branded_food',nix_item_id=fries_id))
-    self.assertTrue('selected value="1.0"' in resp3b.get_data(as_text=True))
+    self.assertTrue('selected value="1.00"' in resp3b.get_data(as_text=True))
     self.assertTrue('value="1.00"' in resp3b.get_data(as_text=True))
 
     # When Form Validates On Submit check redirect
     resp4 = self.client.post(url_for('foods.branded_food',nix_item_id=big_mac_id,data = {
-      'serving_unit':'212.0',
+      'serving_unit':'212.00',
       'serving_qty':777.00
     }),follow_redirects=True)
     self.assertTrue(resp4.status_code==200)
 
     # Filling in form data when request method is GET
-    resp5 = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit=212.0,serving_qty=999.0))
-    self.assertTrue('selected value="212.0"' in resp5.get_data(as_text=True))
+    resp5 = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit=212.00,serving_qty=999.00))
+    self.assertTrue('selected value="212.00"' in resp5.get_data(as_text=True))
     self.assertTrue('value="999.00"' in resp5.get_data(as_text=True))    
 
     # Check nutrition updating according to serving_unit and serving_qty
     # (CHECKED AGAINST NUTRITIONIX DATABASE INFORMATION)
-    resp6a = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit=212.0,serving_qty=1.0))
-    resp6b = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit=212.0,serving_qty=2.0))
+    resp6a = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit=212.00,serving_qty=1.00))
+    resp6b = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id,serving_unit=212.00,serving_qty=2.00))
     data6a = resp6a.get_data(as_text=True)
     data6b = resp6b.get_data(as_text=True)
 
-    self.assertTrue("Calories: 540.0" in data6a)
-    self.assertTrue("Total Carbohydrate: 45.0" in data6a)
-    self.assertTrue("Protein: 25.0" in data6a)
-    self.assertTrue("Sodium: 950.0" in data6a)
-    self.assertTrue("Sugars: 9.0" in data6a)
-    self.assertTrue("Total Fat: 28.0" in data6a)
-    self.assertTrue("Saturated Fat: 10.0" in data6a)
-    self.assertTrue("Dietary Fiber: 3.0" in data6a)
+    self.assertTrue("Calories: 540.00" in data6a)
+    self.assertTrue("Total Carbohydrate: 45.00" in data6a)
+    self.assertTrue("Protein: 25.00" in data6a)
+    self.assertTrue("Sodium: 950.00" in data6a)
+    self.assertTrue("Sugars: 9.00" in data6a)
+    self.assertTrue("Total Fat: 28.00" in data6a)
+    self.assertTrue("Saturated Fat: 10.00" in data6a)
+    self.assertTrue("Dietary Fiber: 3.00" in data6a)
 
-    self.assertTrue("Calories: 1080.0" in data6b)
-    self.assertTrue("Total Carbohydrate: 90.0" in data6b)
-    self.assertTrue("Protein: 50.0" in data6b)
-    self.assertTrue("Sodium: 1900.0" in data6b)
-    self.assertTrue("Sugars: 18.0" in data6b)
-    self.assertTrue("Total Fat: 56.0" in data6b)
-    self.assertTrue("Saturated Fat: 20.0" in data6b)
-    self.assertTrue("Dietary Fiber: 6.0" in data6b)
+    self.assertTrue("Calories: 1080.00" in data6b)
+    self.assertTrue("Total Carbohydrate: 90.00" in data6b)
+    self.assertTrue("Protein: 50.00" in data6b)
+    self.assertTrue("Sodium: 1900.00" in data6b)
+    self.assertTrue("Sugars: 18.00" in data6b)
+    self.assertTrue("Total Fat: 56.00" in data6b)
+    self.assertTrue("Saturated Fat: 20.00" in data6b)
+    self.assertTrue("Dietary Fiber: 6.00" in data6b)
     
     # Make sure that result of get_measures_tuple is passed to select form
     resp7 = self.client.get(url_for('foods.branded_food',nix_item_id=big_mac_id))
     data7 = resp7.get_data(as_text=True)
-    self.assertTrue('<option selected value="212.0">burger</option>' in data7)
-
-class FlaskErrorsTestCase(FlaskClientTestCase):
+    self.assertTrue('<option selected value="212.00">burger</option>' in data7)
