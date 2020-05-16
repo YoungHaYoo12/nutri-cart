925a5d339413b927add71d87df1b3163a61d69d3
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-16 08:00:30 +0000

    Implement user registration functionality.
    - Create RegistrationForm
    - Implement a register view to allow user to register
    - Implement auth/register.html to display registration form
    - Create a link from auth/login to auth/register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,26 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms import ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
   email = StringField('Email', validators=[DataRequired(), Email(), Length(1,64)])
   password = PasswordField('Password', validators=[DataRequired()])
   remember_me = BooleanField('Keep Me Logged In')
   submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Length(1,64), Email()])
+  username = StringField('Username', validators=[DataRequired(), Length(1,64)])
+  password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', message='Passwords Must Match')])
+  password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+  submit = SubmitField('Register')
+
+  def validate_email(self, field):
+    if User.query.filter_by(email=field.data).first():
+      raise ValidationError('Email Already Registered')
+  
+  def validate_username(self, field):
+    if User.query.filter_by(username=field.data).first():
+      raise ValidationError('Username Already In Use')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,24 @@
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required
+from app import db
 from app.auth import auth
 from app.models import User 
-from app.auth.forms import LoginForm
+from app.auth.forms import LoginForm, RegistrationForm
+
+@auth.route('/register',methods=['GET','POST'])
+def register():
+  form = RegistrationForm()
+
+  if form.validate_on_submit():
+    user = User(email=form.email.data,username=form.username.data,password=form.password.data)
+
+    db.session.add(user)
+    db.session.commit()
+
+    flash('Successfully Registered')
+    return redirect(url_for('auth.login'))
+
+  return render_template('auth/register.html', form=form)
 
 @auth.route('/login', methods=['GET','POST'])
 def login():
