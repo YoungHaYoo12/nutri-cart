7794c9ab376709d5fef9be6ae0f8cca68aaf7623
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-16 04:48:12 +0000

    Add basic user model

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+  __tablename__ = 'users'
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(64), unique=True, index=True)
+  email = db.Column(db.String(64), unique=True, index=True)
+
+  password_hash = db.Column(db.String(128))
+
+  @property
+  def password(self):
+    raise AttributeError('password is not a readable attribute')
+  
+  @password.setter
+  def password(self,password):
+    self.password_hash = generate_password_hash(password)
+  
+  def verify_password(self,password):
+    return check_password_hash(self.password_hash,password)
+
+  def __repr__(self):
+    return f"<User {self.username}>"
