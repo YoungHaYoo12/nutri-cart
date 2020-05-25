2a27e8b65846cc14b812cfc50443c1677337fc4c
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-25 02:18:51 +0000

    Add unit testing for FoodItem and Cart

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,8 @@
 import unittest
 from sqlalchemy.exc import IntegrityError
 from app import create_app, db
-from app.models import User
+from app.models import User,FoodItem,Cart
+from decimal import Decimal
 
 class FlaskTestCase(unittest.TestCase):
   def setUp(self):
@@ -68,3 +69,181 @@ class UserModelTestCase(FlaskTestCase):
     with self.assertRaises(IntegrityError):
       db.session.add(u2)
       db.session.commit()
+
+class FoodItemModelTestCase(FlaskTestCase):
+  def test_decimal_fields(self):
+    name = 'food1'
+    nf_calories = Decimal(1.00)
+    nf_total_fat = Decimal(2.00)
+    nf_cholesterol = Decimal(3.00)
+    nf_saturated_fat = Decimal(4.00)
+    nf_sodium = Decimal(5.00)
+    nf_total_carbohydrate = Decimal(6.00)
+    nf_dietary_fiber = Decimal(7.00)
+    nf_sugars = Decimal(8.00)
+    nf_protein = Decimal(9.00)
+    serving_qty = Decimal(10.00)
+    serving_unit='serving'
+    nutrient_multiplier = Decimal(2.00)
+
+    food1 = FoodItem(name=name,
+    nf_calories=nf_calories,
+    nf_total_fat=nf_total_fat,
+    nf_cholesterol=nf_cholesterol,
+    nf_saturated_fat=nf_saturated_fat,
+    nf_sodium=nf_sodium,
+    nf_total_carbohydrate=nf_total_carbohydrate,
+    nf_dietary_fiber=nf_dietary_fiber,
+    nf_sugars=nf_sugars,
+    nf_protein=nf_protein,
+    serving_qty=serving_qty,
+    serving_unit=serving_unit,
+    nutrient_multiplier=nutrient_multiplier
+    )
+
+    # check type decimal
+    self.assertTrue(type(food1.nf_calories) == Decimal)
+    self.assertTrue(type(food1.nf_total_fat) == Decimal)
+    self.assertTrue(type(food1.nf_cholesterol) == Decimal)
+    self.assertTrue(type(food1.nf_saturated_fat) == Decimal)
+    self.assertTrue(type(food1.nf_sodium) == Decimal)
+    self.assertTrue(type(food1.nf_total_carbohydrate) == Decimal)
+    self.assertTrue(type(food1.nf_dietary_fiber) == Decimal)
+    self.assertTrue(type(food1.nf_sugars) == Decimal)
+    self.assertTrue(type(food1.nf_protein) == Decimal)
+    self.assertTrue(type(food1.serving_qty) == Decimal)
+
+    # check whether nutrient_multiplier is multiplied to nutrients
+    self.assertTrue(food1.nf_calories == Decimal(2))
+    self.assertTrue(food1.nf_total_fat == Decimal(4))
+    self.assertTrue(food1.nf_cholesterol == Decimal(6))
+    self.assertTrue(food1.nf_saturated_fat == Decimal(8))
+    self.assertTrue(food1.nf_sodium == Decimal(10))
+    self.assertTrue(food1.nf_total_carbohydrate == Decimal(12))
+    self.assertTrue(food1.nf_dietary_fiber == Decimal(14))
+    self.assertTrue(food1.nf_sugars == Decimal(16))
+    self.assertTrue(food1.nf_protein == Decimal(18))
+
+    # check default value of nutrient_multiplier 
+    name = 'food2'
+    food2 = FoodItem(name=name,
+    nf_calories=nf_calories,
+    nf_total_fat=nf_total_fat,
+    nf_cholesterol=nf_cholesterol,
+    nf_saturated_fat=nf_saturated_fat,
+    nf_sodium=nf_sodium,
+    nf_total_carbohydrate=nf_total_carbohydrate,
+    nf_dietary_fiber=nf_dietary_fiber,
+    nf_sugars=nf_sugars,
+    nf_protein=nf_protein,
+    serving_qty=serving_qty,
+    serving_unit=serving_unit,
+    )    
+    self.assertTrue(food2.nutrient_multiplier == Decimal(1))
+    self.assertTrue(food2.nf_calories == Decimal(1))
+    self.assertTrue(food2.nf_total_fat == Decimal(2))
+    self.assertTrue(food2.nf_cholesterol == Decimal(3))
+    self.assertTrue(food2.nf_saturated_fat == Decimal(4))
+    self.assertTrue(food2.nf_sodium == Decimal(5))
+    self.assertTrue(food2.nf_total_carbohydrate == Decimal(6))
+    self.assertTrue(food2.nf_dietary_fiber == Decimal(7))
+    self.assertTrue(food2.nf_sugars == Decimal(8))
+    self.assertTrue(food2.nf_protein == Decimal(9))
+
+class CartModelTestCase(FlaskTestCase):
+  def test_default_values(self):
+    cart1 = Cart(1)
+
+    # test initial values given to Cart fields 
+    self.assertTrue(cart1.cart_num==1)
+    self.assertTrue(cart1.nf_calories == Decimal(0))
+    self.assertTrue(cart1.nf_total_fat == Decimal(0))
+    self.assertTrue(cart1.nf_saturated_fat == Decimal(0))
+    self.assertTrue(cart1.nf_cholesterol == Decimal(0))
+    self.assertTrue(cart1.nf_sodium == Decimal(0))
+    self.assertTrue(cart1.nf_total_carbohydrate == Decimal(0))
+    self.assertTrue(cart1.nf_dietary_fiber == Decimal(0))
+    self.assertTrue(cart1.nf_sugars == Decimal(0))
+    self.assertTrue(cart1.nf_protein == Decimal(0))
+  
+  def test_update_nutrients(self):
+    food1 = FoodItem(name='food1',
+    nf_calories=Decimal(1),
+    nf_total_fat=Decimal(2),
+    nf_cholesterol=Decimal(3),
+    nf_saturated_fat=Decimal(4),
+    nf_sodium=Decimal(5),
+    nf_total_carbohydrate=Decimal(6),
+    nf_dietary_fiber=Decimal(7),
+    nf_sugars=Decimal(8),
+    nf_protein=Decimal(9),
+    nutrient_multiplier=Decimal(1))
+
+    food2 = FoodItem(name='food2',
+    nf_calories=Decimal(11),
+    nf_total_fat=Decimal(12),
+    nf_cholesterol=Decimal(13),
+    nf_saturated_fat=Decimal(14),
+    nf_sodium=Decimal(15),
+    nf_total_carbohydrate=Decimal(16),
+    nf_dietary_fiber=Decimal(17),
+    nf_sugars=Decimal(18),
+    nf_protein=Decimal(19),
+    nutrient_multiplier=Decimal(1))
+
+    cart2 = Cart(2)
+    food1.cart = cart2
+    food2.cart = cart2
+
+    cart2.update_nutrients()
+    self.assertTrue(cart2.nf_calories == Decimal(12))
+    self.assertTrue(cart2.nf_total_fat == Decimal(14))
+    self.assertTrue(cart2.nf_cholesterol == Decimal(16))
+    self.assertTrue(cart2.nf_saturated_fat == Decimal(18))
+    self.assertTrue(cart2.nf_sodium == Decimal(20))
+    self.assertTrue(cart2.nf_total_carbohydrate == Decimal(22))
+    self.assertTrue(cart2.nf_dietary_fiber == Decimal(24))
+    self.assertTrue(cart2.nf_sugars == Decimal(26))
+    self.assertTrue(cart2.nf_protein == Decimal(28))
+  
+  def test_relationships(self):
+    food1 = FoodItem(name='food1',
+    nf_calories=Decimal(1),
+    nf_total_fat=Decimal(2),
+    nf_cholesterol=Decimal(3),
+    nf_saturated_fat=Decimal(4),
+    nf_sodium=Decimal(5),
+    nf_total_carbohydrate=Decimal(6),
+    nf_dietary_fiber=Decimal(7),
+    nf_sugars=Decimal(8),
+    nf_protein=Decimal(9),
+    nutrient_multiplier=Decimal(1))
+
+    food2 = FoodItem(name='food2',
+    nf_calories=Decimal(11),
+    nf_total_fat=Decimal(12),
+    nf_cholesterol=Decimal(13),
+    nf_saturated_fat=Decimal(14),
+    nf_sodium=Decimal(15),
+    nf_total_carbohydrate=Decimal(16),
+    nf_dietary_fiber=Decimal(17),
+    nf_sugars=Decimal(18),
+    nf_protein=Decimal(19),
+    nutrient_multiplier=Decimal(1))
+
+    cart1 = Cart(1)
+    cart2 = Cart(2)
+
+    user1 = User(email="one@one.com",username="one",password="one")
+    user2 = User(email="two@two.com",username="two",password="two")
+
+    food1.cart = cart1
+    cart1.user = user1
+    food2.cart = cart2
+    cart2.user = user2
+
+    # check relationships
+    self.assertTrue(food1.cart == cart1)
+    self.assertTrue(cart1.user == user1)
+    self.assertTrue(food2.cart == cart2)
+    self.assertTrue(cart2.user == user2)
