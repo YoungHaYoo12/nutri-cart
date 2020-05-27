a8313caf135244564fedd9dc63c8a46642108931
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-27 08:49:34 +0000

    Add carts.add view

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,9 +1,21 @@
+from app import db
 from app.carts import carts
+from app.models import Cart
 from flask_login import current_user,login_required
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 @carts.route('/list/carts')
 @login_required
 def list():
   carts = current_user.carts
   return render_template('carts/list.html',carts=carts)
+
+@carts.route('/add_cart')
+@login_required
+def add():
+  num_of_carts = len(current_user.carts)
+  cart = Cart(num_of_carts + 1)
+  cart.user = current_user
+  db.session.add(cart)
+  db.session.commit()
+  return redirect(url_for('carts.list'))
