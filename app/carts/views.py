e8fb29a2a7106f43cda7582ee7038722687120f5
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-05-27 08:38:43 +0000

    Add carts.list view page and template

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,1 +1,9 @@
 from app.carts import carts
+from flask_login import current_user,login_required
+from flask import render_template
+
+@carts.route('/list/carts')
+@login_required
+def list():
+  carts = current_user.carts
+  return render_template('carts/list.html',carts=carts)
