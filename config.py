1945a6a1b4ff8c46ecb1a9a58234f7974b4b39a8
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-08 14:13:16 +0000

    Set SQLALCHEMY_TRACK_MODIFICATIONS to False

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
   SECRET_KEY = os.environ.get('SECRET_KEY')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
 
   @staticmethod
   def init_app(app):
