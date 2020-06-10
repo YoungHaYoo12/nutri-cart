61f04959b7f138720eee3970b248e2f19b407dd7
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-10 10:17:27 +0000

    Add custom 403 error page

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -4,3 +4,7 @@ from app.errors import errors
 @errors.app_errorhandler(404)
 def not_found_error(error):
   return render_template('errors/404.html'), 404
+
+@errors.app_errorhandler(403)
+def forbidden_error(error):
+  return render_template('errors/403.html'), 403
