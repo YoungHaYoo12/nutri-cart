eba8d9f2fff86b4d0f1212bb915b7642e5e5aa2a
YoungHaYoo12 <youngha.sparky@gmail.com>
2020-06-10 09:47:03 +0000

    Add core.users page.
    - In core/views.py, add the view function for core.users
    
    - In templates/core/users.html, add the templating for page.
    
    - In static/main.css, stylize users.html page.

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for
 from app.core import core 
 from app.core.forms import SearchForm
+from app.models import User
 
 @core.route('/', methods=['GET','POST'])
 def index():
@@ -9,3 +10,14 @@ def index():
     return redirect(url_for('foods.list',food_name=form.query.data,filter="common"))
 
   return render_template('core/index.html',form=form)
+
+@core.route('/users/<username>', methods=['GET','POST'])
+@core.route('/users', methods=['GET','POST'])
+def users(username=None):
+  form = SearchForm()
+  if form.validate_on_submit():
+    return redirect(url_for('core.users', username=form.query.data))
+
+  user = User.query.filter_by(username=username).first()
+
+  return render_template('core/users.html', form=form, query=username,user=user)
